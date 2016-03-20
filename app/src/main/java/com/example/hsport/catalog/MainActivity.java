e2a0b5a9933201d99daaa3ccf63d1c720a55c6c7
paulonova <paulonova144@gmail.com>
2016-03-20 12:02:54 +0100

    Commit 20/03/16 Finish work

--- app/src/main/java/com/example/hsport/catalog/MainActivity.java
+++ app/src/main/java/com/example/hsport/catalog/MainActivity.java
@@ -2,26 +2,41 @@ package com.example.hsport.catalog;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MENU_ITEM_LOGOUT = 1001;
+    public static final String PRODUCT_ID = "PRODUCT_ID";
+    private static final int DETAIL_REQUEST = 111;
+    public static final String RETURN_MESSAGE = "RETURN_MESSAGE";
+
     private CoordinatorLayout coordinatorLayout;
+
+    private static String webUrl = "https://www.facebook.com/paulo.vilanova.12";
+    private static String email = "paulonova144@gmail.com";
+    private List<Product> products = DataProvider.productList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +51,51 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "You pressed the FAB", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Log.i("Send email", "");
+                String[] TO = {email};
+                String[] CC = {email};
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                emailIntent.setData(Uri.parse("mailto:"));
+                emailIntent.setType("text/plain");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+                if(emailIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                    Toast.makeText(MainActivity.this, "Sending email", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        String[] items = getResources().getStringArray(R.array.clothing);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+//                                                                      android.R.id.text1, items);
+
+        ProductListAdapter adapter = new ProductListAdapter(this, R.layout.list_item, products);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                Product product = products.get(position);
+                intent.putExtra(PRODUCT_ID,  product.getProductId());
+
+                startActivityForResult(intent, DETAIL_REQUEST);
             }
         });
 
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,6 +124,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
                 return true;
 
+            case R.id.action_web:
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(webUrl));
+                if(webIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(webIntent);
+                }
+                return true;
+
             case R.id.action_cart:
                 Snackbar.make(coordinatorLayout, "You selected the shopping cart", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                 return true;
@@ -83,4 +143,25 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == DETAIL_REQUEST){
+            if(resultCode == RESULT_OK){
+                String message = data.getStringExtra(RETURN_MESSAGE);
+                Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG).setAction("Go to cart", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Going to cart", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        }
+    }
+
+
+
+
 }
